8212ac85de1a6a38b2b132adf8285e225c4424ad
Suraj <2015suraj.pawar@ves.ac.in>
2020-08-23 23:23:00 -0400

    update GOL in golang

--- code/GO/GOL.go
+++ code/GO/GOL.go
@@ -1,113 +1,104 @@
-// An implementation of Conway's Game of Life.
+//Conways Game of Life in Golang
+//Author: Suraj Pawar
+
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"math/rand"
-	"time"
+    "time"
+    "math/rand"
 )
 
-// Field represents a two-dimensional field of cells.
-type Field struct {
-	s    [][]bool
-	w, h int
-}
+func life (rows int, cols int, some float32, generations int){
 
-// NewField returns an empty field of the specified width and height.
-func NewField(w, h int) *Field {
-	s := make([][]bool, h)
-	for i := range s {
-		s[i] = make([]bool, w)
-	}
-	return &Field{s: s, w: w, h: h}
-}
+    
+    now := make([]int, rows*cols)
+    for i := 0; i < rows * cols; i++ {
+        if rand.Float32() < some{
+            now[i] = 1
+        } else{
+            now[i] = 0
+        }
+    }
+    
+    live(now, rows, cols, generations)
 
-// Set sets the state of the specified cell to the given value.
-func (f *Field) Set(x, y int, b bool) {
-	f.s[y][x] = b
 }
+func helper(a []int, c int, rows int, cols int) int{
+    var res int = 0
+    var size int = rows * cols
+    if c % cols != 0 && cols > 1{
+        res += a[c - 1]
+        if c - cols - 1 > -1{
+            res+= a[c - cols - 1]
+        }
+        if c + cols - 1 < size{
+            res += a[c + cols - 1]
+        }
+    } 
+    if c%cols != cols-1 && cols > 1{
+        res += a[c + 1]
+        if c - cols + 1 > -1{
+            res+= a[c - cols + 1]
+        }
+        if c + cols + 1 < size{
+            res += a[c + cols + 1]
+        }
+    }
+    if c + cols < size{
+        res += a[c + cols]
+    }
+    if c - cols > -1{
+        res += a[c - cols]
+    }
+    return res
+} 
 
-// Alive reports whether the specified cell is alive.
-// If the x or y coordinates are outside the field boundaries they are wrapped
-// toroidally. For instance, an x value of -1 is treated as width-1.
-func (f *Field) Alive(x, y int) bool {
-	x += f.w
-	x %= f.w
-	y += f.h
-	y %= f.h
-	return f.s[y][x]
-}
+func live(a []int, rows int, cols int, generations int){
 
-// Next returns the state of the specified cell at the next time step.
-func (f *Field) Next(x, y int) bool {
-	// Count the adjacent cells that are alive.
-	alive := 0
-	for i := -1; i <= 1; i++ {
-		for j := -1; j <= 1; j++ {
-			if (j != 0 || i != 0) && f.Alive(x+i, y+j) {
-				alive++
-			}
-		}
-	}
-	// Return next state according to the game rules:
-	//   exactly 3 neighbors: on,
-	//   exactly 2 neighbors: maintain current state,
-	//   otherwise: off.
-	return alive == 3 || alive == 2 && f.Alive(x, y)
-}
+    if generations < 1 {
+        return
+    }
+    time.Sleep(1000)
+    for i := 0; i < len(a); i++ {
+        if a[i] == 1{
+            fmt.Print("o ")
+        } else{
+            fmt.Print(" ")
+        }
+        if (i + 1)%cols == 0{
+            fmt.Print("")
+        }
 
-// Life stores the state of a round of Conway's Game of Life.
-type Life struct {
-	a, b *Field
-	w, h int
-}
+    }
+    b := make([]int, len(a))
+    for c := 0; c < len(a); c++ {
+        var neighbors int = helper(a, c, rows, cols)
+        b[c] = a[c]
+        if a[c] == 0{
+            if (neighbors == 3){
+                b[c] =  1 
+            }else{
+                b[c] = 0
+            }
+            
+        } else{
+            if (neighbors == 2) || (neighbors == 3){
+                b[c] =  1 
+            }else{
+                b[c] = 0
+            }
+            
+        }
+    }
+    
+    
+    generations -= 1
+    live(b,rows, cols, generations)
 
-// NewLife returns a new Life game state with a random initial state.
-func NewLife(w, h int) *Life {
-	a := NewField(w, h)
-	for i := 0; i < (w * h / 4); i++ {
-		a.Set(rand.Intn(w), rand.Intn(h), true)
-	}
-	return &Life{
-		a: a, b: NewField(w, h),
-		w: w, h: h,
-	}
-}
-
-// Step advances the game by one instant, recomputing and updating all cells.
-func (l *Life) Step() {
-	// Update the state of the next field (b) from the current field (a).
-	for y := 0; y < l.h; y++ {
-		for x := 0; x < l.w; x++ {
-			l.b.Set(x, y, l.a.Next(x, y))
-		}
-	}
-	// Swap fields a and b.
-	l.a, l.b = l.b, l.a
-}
-
-// String returns the game board as a string.
-func (l *Life) String() string {
-	var buf bytes.Buffer
-	for y := 0; y < l.h; y++ {
-		for x := 0; x < l.w; x++ {
-			b := byte(' ')
-			if l.a.Alive(x, y) {
-				b = '*'
-			}
-			buf.WriteByte(b)
-		}
-		buf.WriteByte('\n')
-	}
-	return buf.String()
 }
 
 func main() {
-	l := NewLife(40, 15)
-	for i := 0; i < 300; i++ {
-		l.Step()
-		fmt.Print("\x0c", l) // Clear screen and print field.
-		time.Sleep(time.Second / 30)
-	}
+ 
+    life(20,50,0.619,200)
 }
